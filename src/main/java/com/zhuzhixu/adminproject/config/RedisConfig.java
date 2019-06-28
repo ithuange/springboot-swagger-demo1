558539c124056ac3b950d648cec1c3cfdf366bdd
Zhu Zhixu <1282956226@qq.com>
2019-06-28 10:02:48 +0800

    设置redis session 的存在时间

--- src/main/java/com/zhuzhixu/adminproject/config/RedisConfig.java
+++ src/main/java/com/zhuzhixu/adminproject/config/RedisConfig.java
@@ -5,14 +5,18 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
 @EnableCaching
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 1800)
 public class RedisConfig extends CachingConfigurerSupport {
     @Bean
     public JedisConnectionFactory jedisConnectionFactory(){
         return new JedisConnectionFactory();
     }
+
+
 
 
 }
