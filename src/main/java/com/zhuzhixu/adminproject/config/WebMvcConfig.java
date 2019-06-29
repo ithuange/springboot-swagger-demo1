9145991c6541dd7083745a3017267c020266b357
Zhu Zhixu <1282956226@qq.com>
2019-06-29 16:17:40 +0800

    修改springmvc拿不到静态资源bug 添加log日志

--- src/main/java/com/zhuzhixu/adminproject/config/WebMvcConfig.java
+++ src/main/java/com/zhuzhixu/adminproject/config/WebMvcConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class IntercepterConfig implements WebMvcConfigurer {
+public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new UserInterceptor()).addPathPatterns("/**")
--- src/main/java/com/zhuzhixu/adminproject/controller/ProductController.java
+++ src/main/java/com/zhuzhixu/adminproject/controller/ProductController.java
@@ -1,8 +1,17 @@
 package com.zhuzhixu.adminproject.controller;
 
+import com.zhuzhixu.adminproject.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ProductController {
+    @Autowired
+    private ProductService productService;
 
+    @RequestMapping("/products")
+    public String products(){
+        return "products";
+    }
 }
